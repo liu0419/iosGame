59653832a245a4542219dc278f6201e843a35713
劉丞晏 <h0985316554@gmail.com>
2025-02-21 09:43:17 +0800

    Initial Commit

---
+++ iosClass/iosClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosClassApp.swift
+//  iosClass
+//
+//  Created by 劉丞晏 on 2025/2/21.
+//
+
+import SwiftUI
+
+@main
+struct iosClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
